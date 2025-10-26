db65d80d6384b9046d694a7cf08c4063a10764cd
FilipDonoval <filip.donoval@gmail.com>
2025-10-26 04:00:13 +0100

    json adb

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,20 +11,36 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+import org.json.JSONObject
 
 
 class MainActivity : ComponentActivity(), Orientation.Listener {
 
     private lateinit var mOrientation: Orientation
+
+    val jsonData = JSONObject("""{
+        "steering": 0,
+        "throttle": 0,
+        "gearUp": 0,
+        "gearDown": 0
+    }""")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +52,14 @@ class MainActivity : ComponentActivity(), Orientation.Listener {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                    buton()
+                    buton(gearChange = ::jsonGearChange)
+                    SliderExample(throttleChange = ::jsonChangeThrottle)
                 }
             }
         }
 
         mOrientation = Orientation(this)
+
     }
 
     override fun onStart() {
@@ -54,11 +72,45 @@ class MainActivity : ComponentActivity(), Orientation.Listener {
         mOrientation.stopListening()
     }
 
+
     override fun onOrientationChanged(pitch: Float, roll: Float) {
         //Log.d("", "pitch: $pitch  |  roll: $roll")
         val rollA = (roll * 100).toInt()
         //var roll2Decimal = rollA / 100.0
-        Log.d("PhoneOutput", "<leftStick>$rollA")
+        //Log.d("PhoneOutput", "<leftStick>$rollA")
+        jsonData.put("steering", rollA)
+        sendJson()
+    }
+
+
+    var lastUpdateTime = System.nanoTime()
+    val targetHz = 500
+    val timePerHz = 1_000_000_000L / targetHz
+    fun sendJson() {
+        val now = System.nanoTime()
+
+        if (now - lastUpdateTime >= timePerHz)
+        {
+            lastUpdateTime = now
+            //Log.d("dsakldjsakdljas","dksajldlaksjdklsaj")
+            Log.d("PhoneOutput", "$jsonData");
+            //Log.d("jjjdasdasdasasdasadsdasads", "${jsonData.toString()}");
+        }
+
+    }
+
+
+    fun jsonChangeThrottle(throttlePos: Float)
+    {
+        Log.d("fdsjklfsadkjl","$throttlePos")
+        jsonData.put("throttle", throttlePos)
+        sendJson()
+    }
+
+    fun jsonGearChange(updown: String, state: Int)
+    {
+        jsonData.put(updown, state)
+        sendJson()
     }
 
 }
@@ -84,38 +136,78 @@ fun GreetingPreview() {
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun buton(modifier: Modifier = Modifier)
+fun buton(modifier: Modifier = Modifier, gearChange: (updown: String, state: Int) -> Unit)
 {
     Button(
 
         onClick = { Log.d("PhoneInput", "10") },
         modifier = Modifier
-            .offset(100.dp, 300.dp)
+            .offset(100.dp, 100.dp)
             .pointerInteropFilter { event ->
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        //Log.d("PhoneInput","50")
-                        Log.d("PhoneOutput", "<throttle>255")
+                        gearChange("gearUp", 1)
+
                     }
                     MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                        //Log.d("PhoneInput", "0")
-                        Log.d("PhoneOutput", "<throttle>0")
+                        gearChange("gearUp", 0)
                     }
                 }
                 true
             }
 
     ) {
-        Text("Filled")
+        Text("Gear Up")
     }
+
 
     Button(
 
-        onClick = { Log.d("PhoneOutput", "<gearup>111") },
+        onClick = { Log.d("PhoneInput", "10") },
         modifier = Modifier
-            .offset(100.dp, 400.dp)
+            .offset(100.dp, 200.dp)
+            .pointerInteropFilter { event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        gearChange("gearDown", 1)
+
+                    }
+                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                        gearChange("gearDown", 0)
+                    }
+                }
+                true
+            }
 
     ) {
-        Text("gear")
+        Text("Gear Down")
     }
+
 }
+
+
+
+
+@Composable
+fun SliderExample(throttleChange: (throttlePos: Float) -> Unit) {
+    var sliderValue by remember {mutableStateOf(0f)}
+
+
+    Slider(
+        value = sliderValue,
+        valueRange = 0f..255f,
+        onValueChange = {
+            sliderValue = it
+            throttleChange(sliderValue)
+        },
+        onValueChangeFinished = {
+            sliderValue = 0f
+            throttleChange(sliderValue)
+        },
+        modifier = Modifier
+            .offset(0.dp,550.dp)
+            .padding(70.dp)
+    )
+
+}
+
